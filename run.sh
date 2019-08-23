fea19ccbd83e9bfea3ee2f0f0543ab4b40125d1e
mkirsche <mjkirsche@gmail.com>
2019-08-23 16:39:40 -0400

    Take parameter from command line for how many reads to keep

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+keepProp=$1
 javac hydroplane/*.java
-java hydroplane.Sieve -v  puc=0.3 localDebug=false fn=/work-zfs/mschatz1/mkirsche/reads/ERR2173373.fastq ofn=uncontained.txt nt=16
+java hydroplane.Sieve -v  puc=$keepProp localDebug=false fn=/work-zfs/mschatz1/mkirsche/reads/ERR2173373.fastq ofn=uncontained_$keepProp.txt nt=2
 
