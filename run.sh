0869a3553c61c908957dd9b83c46ecf70f591252
mkirsche <mjkirsche@gmail.com>
2019-08-23 16:42:07 -0400

    Minor output filename edit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 keepProp=$1
 javac hydroplane/*.java
-java hydroplane.Sieve -v  puc=$keepProp localDebug=false fn=/work-zfs/mschatz1/mkirsche/reads/ERR2173373.fastq ofn=uncontained_$keepProp.txt nt=2
+java hydroplane.Sieve -v  puc=$keepProp localDebug=false fn=/work-zfs/mschatz1/mkirsche/reads/ERR2173373.fastq ofn=uncontained_$keepProp.txt urf=uncontainednames_$keepProp.txt  nt=2
 
