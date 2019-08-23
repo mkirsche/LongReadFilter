57f4f430592cf53d5e4ed82299ddb4abd0afafc4
mkirsche <mjkirsche@gmail.com>
2019-08-23 16:37:58 -0400

    Updated run file to be for running on server

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-./sync.sh
-java hydroplane.Sieve -v  puc=0.2 localDebug=false fn=/home/mkirsche/reads/ERR2173373.fastq ofn=uncontained.txt
+javac hydroplane/*.java
+java hydroplane.Sieve -v  puc=0.3 localDebug=false fn=/work-zfs/mschatz1/mkirsche/reads/ERR2173373.fastq ofn=uncontained.txt nt=16
+
